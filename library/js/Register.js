f03b41fd948259fc5ce971797b8257b67193205c
Nesrine <nesrine.zahrouni@esprit.tn>
2020-06-15 13:27:27 +0200

    register

---
+++ library/js/Register.js
@@ -0,0 +1,15 @@
+function setusers(){
+    var t = JSON.parse(localStorage.getItem("users")) || [];
+    var obj = new Object();
+    obj.id=Math.floor(Math.random()*1000);
+    obj.nom=document.getElementById("username").value;
+    obj.pwd=document.getElementById("pwd").value ;
+    obj.pwd1=document.getElementById("pwd1").value ;
+    obj.email=document.getElementById("email").value;
+    obj.switch=document.getElementById("switch-1").checked;
+    console.log('switch'+ obj.switch);
+    
+    t.push(obj);
+    localStorage.setItem("users",JSON.stringify(t));
+    alert("you have succefully registred")
+    }
