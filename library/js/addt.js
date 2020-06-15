a43b6c5f45f09b5a9ad3efe7016c6dd91976631d
koralih <loutarfiras@live.fr>
2020-06-15 14:58:41 +0100

    fixe login.js

--- library/js/addt.js
+++ library/js/addt.js
@@ -1,7 +1,7 @@
 function setterrains(){
     var t = JSON.parse(localStorage.getItem("addt")) || [];
     var obj = new Object();
-    if (document.getElementById("namet").value!="" && document.getElementById("localisation").value!="" && document.getElementById("Nbr_pr").value!="" && document.getElementById("numt").length ==8 && document.getElementById("prixt").value!=""){
+    if (document.getElementById("namet").value !="" && document.getElementById("localisation").value!="" && document.getElementById("Nbr_pr").value!="" && document.getElementById("numt").length == 8 && document.getElementById("prixt").value!=""){
         obj.id=Math.floor(Math.random()*1000);
         obj.nomt=document.getElementById("namet").value;
         obj.loct=document.getElementById("localisation").value ;
@@ -13,11 +13,25 @@ function setterrains(){
         obj.cafe=document.getElementById("Cafe").checked;
         obj.wifi=document.getElementById("Wifi").checked;
         obj.kids=document.getElementById("Kids").checked;
+        console.log(obj.nomt);
+        console.log(obj.loct);
+        console.log(obj.nbrp);
+        console.log(obj.numt.length);
+        console.log(obj.prixt);
+        
+        
         t.push(obj);
         localStorage.setItem("addt",JSON.stringify(t));
         alert("vous avez ajouter un terrain")
     }
     else{
-        alert("veuillez remplir tous les champs")
+        alert("veuillez remplir tous les champs");
+        console.log('error');
+        
+        console.log(document.getElementById("namet").value);
+        console.log(document.getElementById("localisation").value);
+        console.log(document.getElementById("Nbr_pr").value);
+        console.log(document.getElementById("numt").value);
+        console.log(document.getElementById("prixt").value);
     }
 }
--- library/js/login.js
+++ library/js/login.js
@@ -3,11 +3,21 @@ function verifyuser(){
     var obj = new Object();
     x=document.getElementById("emailLog").value;
     y=document.getElementById("pwdLog").value;
-    var z=t.find( user => user.nom == x && user.pwd == y);
-    if (z){
+    var z=t.find( users => users.email == x && users.pwd == y);
+   
+    
+     
+    if (z.switch == false){
         localStorage.setItem("con", JSON.stringify(z));
         alert("you're loged in")
+        console.log("client");
+        
+
     }
+    else if(z.switch == true)
+    {localStorage.setItem("con", JSON.stringify(z));
+    alert("you're loged in")
+    console.log("propPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");}
     else { alert("verify")
         window.location.href="register.html";}
 }
