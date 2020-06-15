c0880cd8d6e673c60b21f25c994c59294b942f89
koralih <loutarfiras@live.fr>
2020-06-15 13:11:42 +0100

    addterain

---
+++ library/js/addt.js
@@ -0,0 +1,18 @@
+function setterrains(){
+    var t = JSON.parse(localStorage.getItem("addt")) || [];
+    var obj = new Object();
+    obj.id=Math.floor(Math.random()*1000);
+    obj.nomt=document.getElementById("namet").value;
+    obj.loct=document.getElementById("localisation").value ;
+    obj.nbrp=document.getElementById("Nbr_pr").value ;
+    obj.numt=document.getElementById("numt").value;
+    obj.prixt=document.getElementById("prixt").value;
+    obj.park=document.getElementById("Parking").checked;
+    obj.swim=document.getElementById("Swiming").checked;
+    obj.cafe=document.getElementById("Cafe").checked;
+    obj.wifi=document.getElementById("Wifi").checked;
+    obj.kids=document.getElementById("Kids").checked;
+    t.push(obj);
+    localStorage.setItem("addt",JSON.stringify(t));
+    alert("vous avez ajouter un terrain")
+    }
