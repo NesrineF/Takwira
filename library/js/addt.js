9cb3cd562efffe9922edaf41e5fdcd42c4cb55c5
koralih <loutarfiras@live.fr>
2020-06-22 12:42:26 +0100

    addt

--- library/js/addt.js
+++ library/js/addt.js
@@ -22,6 +22,7 @@ function openFile(event) {
 
 function setterrains(){
     var t1 = JSON.parse(localStorage.getItem("con"));
+    var obj1 = new Object();
     
     console.log("rrrrrrrrrrrrr" + t1);
     
@@ -31,7 +32,7 @@ function setterrains(){
     if ((document.getElementById("namet").value !="") && (document.getElementById("villes").value !="") && (document.getElementById("localisation").value!="") && (document.getElementById("Nbr_pr").value!="") && (document.getElementById("numt").value.length == 8) && (document.getElementById("prixt").value!="")){
         obj.id=Math.floor(Math.random()*1000);
         obj.idu=t1.id;
-        console.log(obj1.id);
+        console.log=(obj1.id);
         obj.nomt=document.getElementById("namet").value;
         obj.loct=document.getElementById("localisation").value ;
         obj.nbrp=document.getElementById("Nbr_pr").value ;
@@ -44,8 +45,7 @@ function setterrains(){
         obj.wifi=document.getElementById("Wifi").checked;
         obj.kids=document.getElementById("Kids").checked;   
         var Image = document.getElementById("img").value;
-        obj.img = listimage;
-        console.log(obj.img);     
+        obj.img = listimage;     
         t.push(obj);
         localStorage.setItem("addt", JSON.stringify(t));
         listimage = [];
@@ -63,8 +63,10 @@ function setterrains(){
 var element = "";
 function showImage() {
     var t = JSON.parse(localStorage.getItem("addt")) || [];
+    console.log(t);
     for (var i = 0; i < t.length; i++) {
-        element += `	<div class="project-container">
+        element += `	
+        <div class="project-container">
         <div class="project-header">
             <a class="project-thumb lightbox" href="dummies/property_01.jpg" data-lightbox-group="dummy-projects">
                 <img alt="" src="${t[i].img[0]}">
