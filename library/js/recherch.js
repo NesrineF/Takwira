0c9bf0e669da6331ae61e3ff4cbf7b80e537e1a9
Nesrine <nesrine.zahrouni@esprit.tn>
2020-06-25 15:41:55 +0200

    recuperer id

--- library/js/recherch.js
+++ library/js/recherch.js
@@ -8,12 +8,12 @@ function load() {
     var element = ""
     for (let i = 0; i < t.length; i++) {
         console.log(t[i].prixt);
-        
+        console.log("iddddddddddddddddddddddddddddddddddddd"+t[i].id);
         element += `<li class="property first-in-row odd">
         <a href="#" class="property-thumb">
             <img src="${t[i].img[0]} " alt="">
             <div class="property-content">
-                <h4 class="property-title"><a href="#">${t[i].nomt}</a></h4>
+                <h4 class="property-title"><a href="#" onclick="func(${t[i].id})">${t[i].nomt}</a></h4>
                 <h5 class="property-location">${t[i].villes}</h5>
                 <p class="property-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit.</p>
                 
@@ -44,4 +44,9 @@ function openFile(event) {
 
     };
     reader.readAsDataURL(input.files[0]);
+}
+
+function func(e){
+    console.log(e);
+    
 }
