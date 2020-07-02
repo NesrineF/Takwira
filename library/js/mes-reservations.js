692aac200eedde86d288a8500fa609ae306bac83
koralih <loutarfiras@live.fr>
2020-07-02 12:19:57 +0100

    mesr-reservation

--- library/js/mes-reservations.js
+++ library/js/mes-reservations.js
@@ -1,3 +1,24 @@
-function checked(){
-    
+function checked() {
+    var t = JSON.parse(localStorage.getItem("con"));
+    var t1 = JSON.parse(localStorage.getItem("reserved"));
+    var t2 = JSON.parse(localStorage.getItem("addt"));
+    var x = t2.find(addt => addt.idu == t.id);
+    var z = t1.filter(reserved => reserved.id == x.id);
+    var x = 8;
+    var y = 7;
+
+    for (let index = 0; index < z.length; index++) {
+        console.log("aaaaaaaaaaaaaaaa" + z[index].id);
+        if (z != undefined) {
+            for (let h = 1; x < z.x; h++) {
+                for (let j = 1; y < z.y; j++) {
+                    var l = j.toString();
+                    var k = h.toString();
+                    if ((z[index].temp == k) && (z[index].jour == l)) {
+                        document.getElementById(k + l).checked = true;
+                    }
+                }
+            }
+        }
+    }
 }
